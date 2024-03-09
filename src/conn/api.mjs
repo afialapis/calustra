ceda568ea55ece2c2639afcec5b8f3e4cc20c577
afialapis <devel@afialapis.com>
2024-03-09 01:34:32 +0100

    some simplification

--- src/conn/api.mjs
+++ src/conn/api.mjs
@@ -18,6 +18,87 @@ export function getConnection (configOrSelector, options) {
 
   // is it already a connection?
   if (isCalustraConnection(configOrSelector)) {
+    log.warn(`[calustra] getConnection() called using an actual connection (${configOrSelector.connid}). Returning it.`)
+    return configOrSelector
+  }
+  
+  // check if connection is already cached
+  if (! nocache) {
+    const cachedConn= cacheConnectionGet(configOrSelector)
+    if (cachedConn) {
+      // Lets ignore and rebuild the connection if:
+      //  -- it is closed
+      //  -- options.reset is true
+      if ((!cachedConn.isOpen)  || reset) {
+
+        log.silly(`[calustra] getConnection() cached connection found (${cachedConn.connid}), but, let's reinit it. Still open? ${cachedConn.isOpen}. Reset ${reset}. NoCache? ${nocache}`)
+        
+        // insist to actually close it
+        try { 
+          cachedConn.close() 
+        } catch(_) {}
+
+        uncacheConnection(cachedConn)
+      } else {
+
+        log.silly(`[calustra] getConnection() cached connection found (${cachedConn.connid}). Reusing it.`)
+
+
+        // update connection and refresh cache
+        if (options) {
+          uncacheConnection(cachedConn)
+          cachedConn.updateOptions(options)
+          cacheConnectionSet(cachedConn)
+        }
+
+        return cachedConn
+      }
+    }
+  }
+  
+  if (isCalustraSelector(configOrSelector)) {
+    throw new Error(`[calustra] Could not get cached connection for selector ${configOrSelector}`)
+  }
+  
+  
+  const conn= initConnection(configOrSelector, options)
+  if (nocache!==true) {
+    const cacheKey = cacheConnectionSet(conn)
+    log.silly(`[calustra] getConnection() Initing a new connection (${conn.connid}) and cached it with cache key ${cacheKey}`)
+  } else {
+    log.silly(`[calustra] getConnection() Initing a new connection (${conn.connid}) but not caching it.`)
+  }
+
+  return conn
+}
+
+
+
+export function dropConnection(configOrSelector) {
+  const conn = cacheConnectionGet(configOrSelector) 
+  if (conn) {
+    conn.close()
+    uncacheConnection(conn)
+  }
+}
+
+export function dropConnections() {
+  const conns = cacheConnectionGetAll()
+  conns.map(c => {
+    c.close()
+    //removeConnectionFromCache(c.config)
+  })
+  clearCache()
+}
+
+
+
+
+
+/*
+
+  // is it already a connection?
+  if (isCalustraConnection(configOrSelector)) {
     
     const alreadyConn= configOrSelector
 
@@ -56,72 +137,4 @@ export function getConnection (configOrSelector, options) {
 
     return alreadyConn
   }
-  
-  // is connection already cached
-  const cachedConn= cacheConnectionGet(configOrSelector)
-  if (cachedConn) {
-    // Lets ignore and rebuild the connection if:
-    //  -- it is closed
-    //  -- options.reset is true
-    //  -- options.nocache is true
-    if ((!cachedConn.isOpen) || nocache || reset) {
-
-      log.silly(`[calustra] getConnection() cached connection found (${cachedConn.connid}), but, let's reinit it. Still open? ${cachedConn.isOpen}. Reset ${reset}. NoCache? ${nocache}`)
-      
-      // insist to actually close it
-      try { 
-        cachedConn.close() 
-      } catch(_) {}
-
-      uncacheConnection(cachedConn)
-    } else {
-
-      log.silly(`[calustra] getConnection() cached connection found (${cachedConn.connid}). Reusing it.`)
-
-
-      // update connection and refresh cache
-      if (options) {
-        uncacheConnection(cachedConn)
-        cachedConn.updateOptions(options)
-        cacheConnectionSet(cachedConn)
-      }
-
-      return cachedConn
-    }
-  }
-  
-  if (isCalustraSelector(configOrSelector)) {
-    throw new Error(`[calustra] Could not get cached connection for selector ${configOrSelector}`)
-  }
-  
-  
-  const conn= initConnection(configOrSelector, options)
-  if (nocache!==true) {
-    const cacheKey = cacheConnectionSet(conn)
-    log.silly(`[calustra] getConnection() Initing a new connection (${conn.connid}) and cached it with cache key ${cacheKey}`)
-    
-  } else {
-    log.silly(`[calustra] getConnection() Initing a new connection (${conn.connid}) but not caching it.`)
-  }
-
-  return conn
-}
-
-
-
-export function dropConnection(configOrSelector) {
-  const conn = cacheConnectionGet(configOrSelector) 
-  if (conn) {
-    conn.close()
-    uncacheConnection(conn)
-  }
-}
-
-export function dropConnections() {
-  const conns = cacheConnectionGetAll()
-  conns.map(c => {
-    c.close()
-    //removeConnectionFromCache(c.config)
-  })
-  clearCache()
-}
+*/
